5315e9f393ee98894f904443b8af1eaa64027949
ZIBER <ZIBER>
2016-12-19 21:52:08 +0300

    yuiy

---
+++ RunningButton/RunningButton/msBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningButton
+{
+    class msBox
+    {
+
+        public void showMSB(string text,string caption)
+        {
+            MessageBox.Show(text,caption);
+        }
+    }
+}
