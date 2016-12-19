b6f43edea47a19785b4e6a38070d2c04eb91f473
ZIBER <ZIBER>
2016-12-19 21:31:11 +0300

    Second branch

--- RunningButton/RunningButton/Program.cs
+++ RunningButton/RunningButton/Program.cs
@@ -17,7 +17,7 @@ namespace RunningButton
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            MessageBox.Show("fsd","Test");
+            MessageBox.Show("New branch","Test");
         }
 
     }
