7db6c50036291359b96239aedb797a89d9cc3ead
ZIBER <ZIBER>
2016-12-19 21:40:35 +0300

    новый класс msBox

--- RunningButton/RunningButton/Program.cs
+++ RunningButton/RunningButton/Program.cs
@@ -18,6 +18,8 @@ namespace RunningButton
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             MessageBox.Show("first branch","Test");
+            msBox mb = new msBox();
+            mb.showMSB("Новая ветка","ветка");
         }
 
     }
